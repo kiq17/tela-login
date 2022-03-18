fe22aa2a2bdeeaa9ac02a07434dd8dfcdba661e1
Caique <101359008+kiq17@users.noreply.github.com>
2022-03-18 20:49:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-function toogle(){
-    var tipo = document.getElementById('password');
-    var btn = document.getElementById('eye');
-    var btn2 = document.getElementById('esconde');
+const tipo = document.getElementById('password');
+const btnMostrar = document.getElementById('mostrar');
+const btnEsconder = document.getElementById('esconde');
 
-    if(tipo.type == 'password'){
-        tipo.type = "text";
-        btn.classList.toggle('ativo');
-        btn2.classList.toggle('ativou');
+function mostrarSenha() {
+
+    if (tipo.type == 'password') {
+        tipo.type = 'text';
+        btnMostrar.style.display = 'block'
+        btnEsconder.style.display = 'none'
     }
-    else{
-        tipo.type = "password";
-        btn.classList.toggle('ativo');
-        btn2.classList.toggle('ativou');
+    else {
+        tipo.type = 'password';
+        btnMostrar.style.display = 'none'
+        btnEsconder.style.display = 'block'
+
     }
+}
+
+carregarEventos();
+function carregarEventos() {
+    btnMostrar.addEventListener('click', mostrarSenha)
+    btnEsconder.addEventListener('click', mostrarSenha)
 }
     
